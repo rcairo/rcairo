b8ba24271de98ce7c484fbf244454d3369aa479d
Kouhei Sutou <kou@clear-code.com>
2005-09-29 06:53:47 +0000

    * packages/cairo/ext/extconf.rb: check modversion. * packages/svgcairo/ext/extconf.rb: ditto.

--- packages/cairo/ext/extconf.rb
+++ packages/cairo/ext/extconf.rb
@@ -2,8 +2,25 @@
 # vim: filetype=ruby:expandtab:shiftwidth=4:tabstop=8:softtabstop=4 :
 
 require 'mkmf'
+require 'fileutils'
 
-$CFLAGS  = "-Wall "+`pkg-config --cflags cairo`
-$LDFLAGS = `pkg-config --libs cairo`
-create_makefile("cairo")
+pkg = "cairo"
+modname = "cairo"
+major, minor, micro = 1, 0, 0
 
+check_version = Proc.new do
+  modversion = `#{$PKGCONFIG} --modversion #{pkg}`.chomp
+  ver = modversion.split(".").collect{|item| item.to_i}
+  (0..2).each {|i| ver[i] ||= 0}
+  (ver <=> [major, minor, micro]) >= 0
+end
+
+STDOUT.print("checking for #{pkg} version (>= #{major}.#{minor}.#{micro})... ")
+
+if pkg_config(pkg) and check_version.call
+  STDOUT.print("yes\n")
+  create_makefile(modname)
+else
+  STDOUT.print("no\n")
+  FileUtils.rm_f("Makefile")
+end
