1ba8adaa1b915432963475e93311d7cdd3024603
Kouhei Sutou <kou@clear-code.com>
2005-10-04 03:08:41 +0000

    Support Win32 platform.
    * packages/cairo/ext/rb_cairo.h: Support Win32 platform.
    * packages/cairo/ext/extconf.rb: ditto.
    * packages/cairo/ext/cairo.def: Added.
    * packages/cairo/ext/pkg-config.rb: Added. This file is from
      Ruby/GLib.

--- packages/cairo/ext/extconf.rb
+++ packages/cairo/ext/extconf.rb
@@ -1,26 +1,67 @@
 #!/usr/bin/env ruby
-# vim: filetype=ruby:expandtab:shiftwidth=4:tabstop=8:softtabstop=4 :
+# vim: filetype=ruby:expandtab:shiftwidth=2:tabstop=8:softtabstop=2 :
 
 require 'mkmf'
+require 'pkg-config'
 require 'fileutils'
+
+# from mkmf-gnome2.rb
+STDOUT.print("checking for GCC... ")
+STDOUT.flush
+if macro_defined?("__GNUC__", "")
+  STDOUT.print "yes\n"
+  $CFLAGS += ' -Wall' 
+  $cc_is_gcc = true
+else
+  STDOUT.print "no\n"
+  $cc_is_gcc = false
+end
+
+def check_win32()
+  STDOUT.print("checking for Win32 OS... ")
+  STDOUT.flush
+  if /mingw/ =~ RUBY_PLATFORM
+    STDOUT.print "yes\n"
+    if $cc_is_gcc
+      if /^2\./ =~ `#{Config::CONFIG['CC']} -dumpversion`.chomp
+        $CFLAGS += ' -fnative-struct'
+      else
+        $CFLAGS += ' -mms-bitfields'
+      end
+    end
+  else
+    STDOUT.print "no\n"
+  end
+end
+
+def set_output_lib(target_name)
+  filename = "libruby-#{target_name}.a"
+  if /cygwin|mingw/ =~ RUBY_PLATFORM
+    $defs << "-DRUBY_CAIRO_PLATFORM_WIN32"
+    if RUBY_VERSION > "1.8.0"
+      $DLDFLAGS << ",--out-implib=#{filename}" if filename
+    elsif RUBY_VERSION > "1.8"
+      CONFIG["DLDFLAGS"].gsub!(/ -Wl,--out-implib=[^ ]+/, '')
+      CONFIG["DLDFLAGS"] << " -Wl,--out-implib=#{filename}" if filename
+    else
+      CONFIG["DLDFLAGS"].gsub!(/ --output-lib\s+[^ ]+/, '')
+      CONFIG["DLDFLAGS"] << " --output-lib #{filename}" if filename
+    end
+  end
+end
+
+def setup_win32(target_name)
+  check_win32
+  set_output_lib(target_name)
+end
+
 
 pkg = "cairo"
 modname = "cairo"
 major, minor, micro = 1, 0, 0
 
-check_version = Proc.new do
-  modversion = `#{$PKGCONFIG} --modversion #{pkg}`.chomp
-  ver = modversion.split(".").collect{|item| item.to_i}
-  (0..2).each {|i| ver[i] ||= 0}
-  (ver <=> [major, minor, micro]) >= 0
-end
+PKGConfig.have_package(pkg, major, minor, micro) or exit 1
 
-STDOUT.print("checking for #{pkg} version (>= #{major}.#{minor}.#{micro})... ")
-
-if pkg_config(pkg) and check_version.call
-  STDOUT.print("yes\n")
-  create_makefile(modname)
-else
-  STDOUT.print("no\n")
-  FileUtils.rm_f("Makefile")
-end
+setup_win32(modname)
+$defs << "-DRUBY_CAIRO_COMPILATION"
+create_makefile(modname)
