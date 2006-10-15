9473d67f4f6b3286f083dc9945710a22d81623e6
Kouhei Sutou <kou@clear-code.com>
2006-10-15 06:49:04 +0000

    * packages/cairo/lib/cairo.rb (Cairo::Context#quad_to,   Cairo::Context#rel_quad_to): followed the current API:   get_point -> current_point.

--- packages/cairo/lib/cairo.rb
+++ packages/cairo/lib/cairo.rb
@@ -29,16 +29,16 @@ module Cairo
     Cairo.__add_one_arg_setter(self)
 
     def quad_to(x1, y1, x2, y2)
-      ( x0, y0 ) = get_point
-      cx1 = x0 + 2 * ( x1 - x0 ) / 3
-      cy1 = y0 + 2 * ( y1 - y0 ) / 3
-      cx2 = cx1 + ( x2 - x0 ) / 3
-      cy2 = cy1 + ( y2 - y0 ) / 3
+      x0, y0 = current_point
+      cx1 = x0 + 2 * (x1 - x0) / 3
+      cy1 = y0 + 2 * (y1 - y0) / 3
+      cx2 = cx1 + (x2 - x0) / 3
+      cy2 = cy1 + (y2 - y0) / 3
       curve_to(cx1, cy1, cx2, cy2, x2, y2)
     end
     
     def rel_quad_to(x1, y1, x2, y2)
-      ( x0, y0 ) = get_point
+      x0, y0 = current_point
       quad_to(x1 + x0, y1 + y0, x2 + x0, y2 + x0)
     end
 
