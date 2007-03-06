cd4e9d11af87d7f3fb1286503320700707277d5f
Kouhei Sutou <kou@clear-code.com>
2007-03-06 13:01:47 +0000

    * dist.sh: added release script.

---
+++ dist.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+if [ $# = 0 ]; then
+    echo "usage: $0 VERSION"
+    echo "e.g. $0 1.6.0"
+    exit
+fi
+
+VERSION=$1
+TAG=RELEASE_`echo $VERSION | sed -e 's/\./_/g'`
+
+dest=gabe.freedesktop.org:/home/srv/cairo.freedesktop.org/www/releases/
+
+base=rcairo-$VERSION
+tgz=$base.tar.gz
+
+
+cvs tag $TAG
+mkdir -p tmp
+cvs export -r $TAG -d tmp/$base rcairo
+tar cvfz $tgz -C tmp $base
+md5sum $tgz > $tgz.md5
+sha1sum $tgz > $tgz.sha1
+rm -rf tmp
+
+scp $tgz $tgz.md5 $tgz.sha1 $dest
