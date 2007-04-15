665b4ebc86d629f922410914abf2619e945182ce
kou <kou>
2007-04-15 06:15:22 +0000

    * src/lib/cairo/context.rb, src/lib/cairo/context/blur.rb: added   Cairo::Context#pseudo_blur.
    * samples/blur.rb: added a sample script for Cairo::Context#pseudo_blur.

--- src/lib/cairo/context.rb
+++ src/lib/cairo/context.rb
@@ -2,6 +2,7 @@ require 'cairo/context/quad'
 require 'cairo/context/rectangle'
 require 'cairo/context/circle'
 require 'cairo/context/path'
+require 'cairo/context/blur'
 
 module Cairo
   class Context
@@ -9,5 +10,6 @@ module Cairo
     include Rectangle
     include Circle
     include Path
+    include Blur
   end
 end
