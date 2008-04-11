9944714e423dabc82b33feb0f463799133fc26b8
kou <kou>
2008-04-11 02:39:12 +0000

    * src/lib/cairo/paper.rb, test/test_paper.rb: use pt as default unit.

--- test/test_paper.rb
+++ test/test_paper.rb
@@ -24,10 +24,10 @@ class PaperTest < Test::Unit::TestCase
 
   def test_parse_size
     assert_parse(paper(100, 200), "100x200")
-    assert_parse(paper(100, 200), "100mmx200mm")
-    assert_parse(paper(100.5, 200.9), "100.5mmx200.9")
-    assert_parse(paper(100.5, 200.9), "100.5mmx200.9")
-    assert_parse(paper(25.4, 215.9), "1inchx8.5inch")
+    assert_parse(paper(283.46456664, 566.929134), "100mmx200mm")
+    assert_parse(paper(284.88188952, 200.9), "100.5mmx200.9")
+    assert_parse(paper(72, 612.0), "1inx8.5inch")
+    assert_parse(paper(28.346456664, 24094.488168), "1cmx8.5m")
     assert_parse(paper(100, 200), [100, 200])
 
     exception = assert_raise(Cairo::Paper::UnknownUnit) do
@@ -47,14 +47,47 @@ class PaperTest < Test::Unit::TestCase
     assert_equal({}, exception.description)
   end
 
+  def test_unit
+    paper = parse("1cmx8.5m")
+    assert_nil(paper.unit)
+    assert_in_delta(28.346456664, 0.01, paper.width)
+    assert_in_delta(24094.488168, 0.01, paper.height)
+
+    paper.unit = "inch"
+    assert_equal("inch", paper.unit)
+    assert_in_delta(0.393700787, 0.01, paper.width)
+    assert_in_delta(334.645669, 0.01, paper.height)
+  end
+
   private
   def paper(width, height)
     Cairo::Paper.new(width, height)
   end
 
-  def assert_parse(expected, paper_description)
+  def parse(paper_description)
+    Cairo::Paper.parse(paper_description)
+  end
+
+  def assert_parse(expected, paper_description, message=nil)
     expected = Cairo::Paper.const_get(expected) if expected.is_a?(Symbol)
-    actual_paper = Cairo::Paper.parse(paper_description)
-    assert_equal(expected.to_s, actual_paper.nil? ? nil : actual_paper.to_s)
+    actual_paper = parse(paper_description)
+    assert_equal_paper(expected, actual_paper, message)
+  end
+
+  def assert_equal_paper(expected, actual, message=nil)
+    delta = 0.01
+    assert_block(build_message(message,
+                               "<?> expected but was\n<?>.",
+                               expected, actual)) do
+      if actual.nil?
+        expected.nil?
+      else
+        (expected.width - delta < actual.width and
+         actual.width < expected.width + delta) and
+          (expected.height - delta < actual.height and
+           actual.height < expected.height + delta) and
+          expected.name == actual.name
+      end
+    end
   end
 end
