a424d92c36f1d5d1e84c15c9c6595a775852d913
kou <kou>
2008-08-13 08:22:14 +0000

    * test/run-test.rb: use Test::Unit 2.x.

--- test/run-test.rb
+++ test/run-test.rb
@@ -1,14 +1,25 @@
 #!/usr/bin/env ruby
 
 base_dir = File.expand_path(File.join(File.dirname(__FILE__), ".."))
+test_unit_dir = File.join(base_dir, "test-unit", "lib")
 src_dir = File.join(base_dir, "src")
 lib_dir = File.join(src_dir, "lib")
+test_dir = File.join(base_dir, "test")
+
+if system("which make > /dev/null")
+  system("cd #{base_dir.dump} && make > /dev/null") or exit(1)
+end
+
+$LOAD_PATH.unshift(test_unit_dir)
+
+require 'test/unit'
 
 $LOAD_PATH.unshift(base_dir)
 $LOAD_PATH.unshift(src_dir)
 $LOAD_PATH.unshift(lib_dir)
 
-require 'test/unit'
+$LOAD_PATH.unshift(test_dir)
+require 'cairo-test-utils'
 
 Dir.glob("test/**/test_*.rb") do |file|
   require file.sub(/\.rb$/, '')
