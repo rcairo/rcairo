eec7e90c41b28e66b8f30ca2790e2a88294eabef
Kouhei Sutou <kou@clear-code.com>
2008-08-17 05:12:37 +0000

    * src/rb_cairo_font_extents.c: add Cairo::FontExtents#initialize         and setters.
            * test/test_font_extents.rb: add.

--- src/rb_cairo_font_extents.c
+++ src/rb_cairo_font_extents.c
@@ -3,7 +3,7 @@
  * Ruby Cairo Binding
  *
  * $Author: kou $
- * $Date: 2007-05-03 02:47:39 $
+ * $Date: 2008-08-17 05:12:37 $
  *
  * Copyright 2005 Øyvind Kolås <pippin@freedesktop.org>
  * Copyright 2004-2005 MenTaLguY <mental@rydia.com>
@@ -46,34 +46,92 @@ rb_cairo_font_extents_to_ruby_object (cairo_font_extents_t *extents)
     }
 }
 
-static    VALUE
+static VALUE
+cr_font_extents_allocate (VALUE klass)
+{
+  return Data_Wrap_Struct (klass, NULL, -1, NULL);
+}
+
+static VALUE
+cr_font_extents_initialize (VALUE self)
+{
+  cairo_font_extents_t *extents;
+
+  extents = ALLOC (cairo_font_extents_t);
+  extents->ascent = 1.0;
+  extents->descent = 0.0;
+  extents->height = 1.0;
+  extents->max_x_advance = 1.0;
+  extents->max_y_advance = 0.0;
+
+  DATA_PTR (self) = extents;
+
+  return Qnil;
+}
+
+static VALUE
 cr_font_extents_ascent (VALUE self)
 {
   return rb_float_new (_SELF(self)->ascent);
 }
 
-static    VALUE
+static VALUE
+cr_font_extents_set_ascent (VALUE self, VALUE ascent)
+{
+  _SELF(self)->ascent = NUM2DBL (ascent);
+  return self;
+}
+
+static VALUE
 cr_font_extents_descent (VALUE self)
 {
   return rb_float_new (_SELF(self)->descent);
 }
 
-static    VALUE
+static VALUE
+cr_font_extents_set_descent (VALUE self, VALUE descent)
+{
+  _SELF(self)->descent = NUM2DBL (descent);
+  return self;
+}
+
+static VALUE
 cr_font_extents_height (VALUE self)
 {
   return rb_float_new (_SELF(self)->height);
 }
 
-static    VALUE
+static VALUE
+cr_font_extents_set_height (VALUE self, VALUE height)
+{
+  _SELF(self)->height = NUM2DBL (height);
+  return self;
+}
+
+static VALUE
 cr_font_extents_max_x_advance (VALUE self)
 {
   return rb_float_new (_SELF(self)->max_x_advance);
 }
 
-static    VALUE
+static VALUE
+cr_font_extents_set_max_x_advance (VALUE self, VALUE max_x_advance)
+{
+  _SELF(self)->max_x_advance = NUM2DBL (max_x_advance);
+  return self;
+}
+
+static VALUE
 cr_font_extents_max_y_advance (VALUE self)
 {
   return rb_float_new (_SELF(self)->max_y_advance);
+}
+
+static VALUE
+cr_font_extents_set_max_y_advance (VALUE self, VALUE max_y_advance)
+{
+  _SELF(self)->max_y_advance = NUM2DBL (max_y_advance);
+  return self;
 }
 
 static VALUE
@@ -110,16 +168,33 @@ Init_cairo_font_extents (void)
   rb_cCairo_FontExtents =
     rb_define_class_under (rb_mCairo, "FontExtents", rb_cObject);
 
+  rb_define_alloc_func (rb_cCairo_FontExtents, cr_font_extents_allocate);
+
+  rb_define_method (rb_cCairo_FontExtents, "initialize",
+                    cr_font_extents_initialize, 0);
+
   rb_define_method (rb_cCairo_FontExtents, "ascent",
                     cr_font_extents_ascent, 0);
+  rb_define_method (rb_cCairo_FontExtents, "set_ascent",
+                    cr_font_extents_set_ascent, 1);
   rb_define_method (rb_cCairo_FontExtents, "descent",
                     cr_font_extents_descent, 0);
+  rb_define_method (rb_cCairo_FontExtents, "set_descent",
+                    cr_font_extents_set_descent, 1);
   rb_define_method (rb_cCairo_FontExtents, "height",
                     cr_font_extents_height, 0);
+  rb_define_method (rb_cCairo_FontExtents, "set_height",
+                    cr_font_extents_set_height, 1);
   rb_define_method (rb_cCairo_FontExtents, "max_x_advance",
                     cr_font_extents_max_x_advance, 0);
+  rb_define_method (rb_cCairo_FontExtents, "set_max_x_advance",
+                    cr_font_extents_set_max_x_advance, 1);
   rb_define_method (rb_cCairo_FontExtents, "max_y_advance",
                     cr_font_extents_max_y_advance, 0);
+  rb_define_method (rb_cCairo_FontExtents, "set_max_y_advance",
+                    cr_font_extents_set_max_y_advance, 1);
 
   rb_define_method (rb_cCairo_FontExtents, "to_s", cr_font_extents_to_s, 0);
+
+  RB_CAIRO_DEF_SETTERS (rb_cCairo_FontExtents);
 }
