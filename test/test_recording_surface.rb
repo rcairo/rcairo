5347585ddfd10d423433149c27ccd02d3c09ce6a
Kouhei Sutou <kou@clear-code.com>
2011-05-27 21:45:28 +0900

    add a comment to 1f517d3 change. (it has wrong URL...)
    https://bugs.freedesktop.org/show_bug.cgi?id=24688 is the correct URL.

--- test/test_recording_surface.rb
+++ test/test_recording_surface.rb
@@ -13,6 +13,9 @@ class RecordingSurfaceTest < Test::Unit::TestCase
       context.line_to(80, 100)
       context.stroke
     end
+    # https://bugs.freedesktop.org/show_bug.cgi?id=24688
+    # causes expected value change. It is introduced between
+    # cairo 1.10.0 and 1.10.2.
     assert_equal([14.0, 29.0, 67.0, 72.0], surface.ink_extents)
   end
 end
