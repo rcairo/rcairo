1f517d36feb65d86fe82b21980d20b59858a3a65
Kouhei Sutou <kou@clear-code.com>
2011-05-27 21:38:25 +0900

    [test][surface][recording] fix expected value. close #4
    This expected value change is cuased by the bug fix in cairo:
    https://bugs.freedesktop.org/show_bug.cgi?id=24691

--- test/test_recording_surface.rb
+++ test/test_recording_surface.rb
@@ -13,6 +13,6 @@ class RecordingSurfaceTest < Test::Unit::TestCase
       context.line_to(80, 100)
       context.stroke
     end
-    assert_equal([10.0, 20.0, 99.0, 109.0], surface.ink_extents)
+    assert_equal([14.0, 29.0, 67.0, 72.0], surface.ink_extents)
   end
 end
