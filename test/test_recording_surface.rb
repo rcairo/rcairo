e073f59239f120fe5eb07adca991a60dddd9356f
Kouhei Sutou <kou@clear-code.com>
2012-03-25 15:47:48 +0900

    test surface recoding: follow recoding surface change since 1.12.0

--- test/test_recording_surface.rb
+++ test/test_recording_surface.rb
@@ -6,6 +6,7 @@ class RecordingSurfaceTest < Test::Unit::TestCase
   end
 
   def test_new
+    only_cairo_version(1, 12, 0)
     surface = Cairo::RecordingSurface.new(10, 20, 300, 400)
     assert_equal([0.0, 0.0, 0.0, 0.0], surface.ink_extents)
     Cairo::Context.new(surface) do |context|
@@ -13,9 +14,6 @@ class RecordingSurfaceTest < Test::Unit::TestCase
       context.line_to(80, 100)
       context.stroke
     end
-    # https://bugs.freedesktop.org/show_bug.cgi?id=24688
-    # causes expected value change. It is introduced between
-    # cairo 1.10.0 and 1.10.2.
-    assert_equal([14.0, 29.0, 67.0, 72.0], surface.ink_extents)
+    assert_equal([10.0, 20.0, 61.0, 61.0], surface.ink_extents)
   end
 end
