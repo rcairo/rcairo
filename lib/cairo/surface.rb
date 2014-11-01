0c6f1630e227ac6b1c7e7f2f14a5d3f5332b2be5
Kouhei Sutou <kou@clear-code.com>
2014-11-01 16:21:15 +0900

    Surface#supported?: support "QuartzImage" style type name

--- lib/cairo/surface.rb
+++ lib/cairo/surface.rb
@@ -2,7 +2,9 @@ module Cairo
   class Surface
     class << self
       def supported?(type)
-        supported_predicate = "#{type.to_s.downcase}_supported?"
+        type_components = type.to_s.split(/([A-Z][a-z\d]+)/).reject(&:empty?)
+        snake_case_type_name = type_components.join("_").downcase
+        supported_predicate = "#{snake_case_type_name}_supported?"
         return false unless respond_to?(supported_predicate)
         send(supported_predicate)
       end
