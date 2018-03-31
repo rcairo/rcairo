f22589e2313ff712c87cbfca16fcef47d4c54f33
Kouhei Sutou <kou@clear-code.com>
2018-03-31 23:49:47 +0900

    windows: make re-provisionable

--- build/build-windows.sh
+++ build/build-windows.sh
@@ -20,97 +20,114 @@ run()
   fi
 }
 
-run sudo systemctl stop apt-daily.service apt-daily.timer
+if [ ! -f ~/setup.timestamp ]; then
+  run sudo systemctl stop apt-daily.service apt-daily.timer
 
-run sudo sed -i'' -e 's,http://us,http://jp,g' /etc/apt/sources.list
-run sudo apt update
+  run sudo sed -i'' -e 's,http://us,http://jp,g' /etc/apt/sources.list
+  run sudo apt update
 
-sudo rm -rf /etc/udev/rules.d/70-persistent-net.rules
+  sudo rm -rf /etc/udev/rules.d/70-persistent-net.rules
 
-echo ttf-mscorefonts-installer \
-     msttcorefonts/accepted-mscorefonts-eula \
-     select true | \
-  run sudo debconf-set-selections
+  echo ttf-mscorefonts-installer \
+       msttcorefonts/accepted-mscorefonts-eula \
+       select true | \
+    run sudo debconf-set-selections
 
-run \
-  sudo apt install -y -V \
-  git \
-  gperf \
-  ruby \
-  ruby-dev \
-  build-essential \
-  pkg-config \
-  autoconf \
-  libtool \
-  gettext \
-  intltool \
-  gtk-doc-tools \
-  libffi-dev \
-  libglib2.0-dev \
-  gobject-introspection \
-  flex \
-  bison \
-  python-dev \
-  wine1.6 \
-  mingw-w64 \
-  libmount-dev
-
-# run sudo update-alternatives --set \
-#     ${BUILD_HOST}-gcc /usr/bin/${BUILD_HOST}-gcc-posix
-# run sudo update-alternatives --set \
-#     ${BUILD_HOST}-g++ /usr/bin/${BUILD_HOST}-g++-posix
-
-run git clone file:///pkg-config/.git
-run git clone file:///native-package-installer/.git
-run git clone file:///rcairo/.git rcairo.${DIRECTORY_SUFFIX}
-run git clone file:///ruby-gnome2/.git ruby-gnome2.${DIRECTORY_SUFFIX}
-
-run sudo gem install --no-document \
-    rake \
-    bundler \
+  run \
+    sudo apt install -y -V \
+    git \
+    gperf \
+    ruby \
+    ruby-dev \
+    build-essential \
     pkg-config \
-    native-package-installer \
-    rake-compiler \
-    mechanize \
-    packnga
+    autoconf \
+    libtool \
+    gettext \
+    intltool \
+    gtk-doc-tools \
+    libffi-dev \
+    libglib2.0-dev \
+    gobject-introspection \
+    flex \
+    bison \
+    python-dev \
+    wine1.6 \
+    mingw-w64 \
+    libmount-dev
 
-if [ ! -d ~/.wine/ ]; then
-  run wineboot
-  until [ -f ~/.wine/system.reg ]; do
-    sleep 1
-  done
-  sleep 10
-  wine_home="z:/home/vagrant"
-  wine_rcairo="${wine_home}/rcairo.${DIRECTORY_SUFFIX}"
-  wine_ruby_gnome2="${wine_home}/ruby-gnome2.${DIRECTORY_SUFFIX}"
-  bin_dir="vendor/local/bin"
-  path=$(
-    (
-      echo -n "${wine_rcairo}/${bin_dir};";
-      echo -n "${wine_ruby_gnome2}/glib2/${bin_dir};";
-    ) | \
-      sed -e 's,/,\\\\\\\\,g')
-  run sed -i'' -r \
-      -e "s,^(\"PATH\"=str\\(2\\):\"),\\1${path},g" \
-      ~/.wine/system.reg
+  # run sudo update-alternatives --set \
+  #     ${BUILD_HOST}-gcc /usr/bin/${BUILD_HOST}-gcc-posix
+  # run sudo update-alternatives --set \
+  #     ${BUILD_HOST}-g++ /usr/bin/${BUILD_HOST}-g++-posix
+
+  run git clone file:///pkg-config/.git
+  run git clone file:///native-package-installer/.git
+  run git clone file:///rcairo/.git rcairo.${DIRECTORY_SUFFIX}
+  run git clone file:///ruby-gnome2/.git ruby-gnome2.${DIRECTORY_SUFFIX}
+
+  run sudo gem install --no-document \
+      rake \
+      bundler \
+      pkg-config \
+      native-package-installer \
+      rake-compiler \
+      mechanize \
+      packnga
+
+  if [ ! -d ~/.wine/ ]; then
+    run wineboot
+    until [ -f ~/.wine/system.reg ]; do
+      sleep 1
+    done
+    sleep 10
+    wine_home="z:/home/vagrant"
+    wine_rcairo="${wine_home}/rcairo.${DIRECTORY_SUFFIX}"
+    wine_ruby_gnome2="${wine_home}/ruby-gnome2.${DIRECTORY_SUFFIX}"
+    bin_dir="vendor/local/bin"
+    path=$(
+      (
+        echo -n "${wine_rcairo}/${bin_dir};";
+        echo -n "${wine_ruby_gnome2}/glib2/${bin_dir};";
+      ) | \
+        sed -e 's,/,\\\\\\\\,g')
+    run sed -i'' -r \
+        -e "s,^(\"PATH\"=str\\(2\\):\"),\\1${path},g" \
+        ~/.wine/system.reg
+  fi
+
+  run touch ~/setup.timestamp
 fi
 
-sudo chown -R "${USER}:${USER}" /var/lib/gems
+if [ ! -f ~/rake-compiler.timestamp ]; then
+  sudo chown -R "${USER}:${USER}" /var/lib/gems
 
-for ruby_version in ${ruby_versions}; do
-  run rake-compiler cross-ruby HOST="${BUILD_HOST}" VERSION="${ruby_version}"
-done
+  for ruby_version in ${ruby_versions}; do
+    run rake-compiler cross-ruby HOST="${BUILD_HOST}" VERSION="${ruby_version}"
+  done
+
+  run touch ~/rake-compiler.timestamp
+fi
+
+if [ ! -f ~/native.timestamp ]; then
+  run cd ~/rcairo.${DIRECTORY_SUFFIX}
+  run git pull --rebase
+  run rake windows:gcc:dll:bundle windows:zlib:build
+
+  run cd ~/ruby-gnome2.${DIRECTORY_SUFFIX}
+  run git pull --rebase
+
+  run cd ~/ruby-gnome2.${DIRECTORY_SUFFIX}/glib2
+  run rake native:build windows:build
+
+  run cd ~/ruby-gnome2.${DIRECTORY_SUFFIX}/gobject-introspection
+  run rake native:build
+
+  run touch ~/native.timestamp
+fi
 
 run cd ~/rcairo.${DIRECTORY_SUFFIX}
-run rake windows:gcc:dll:bundle windows:zlib:build
-
-run cd ~/ruby-gnome2.${DIRECTORY_SUFFIX}/glib2
-run rake native:build windows:build
-
-run cd ~/ruby-gnome2.${DIRECTORY_SUFFIX}/gobject-introspection
-run rake native:build
-
-run cd ~/rcairo.${DIRECTORY_SUFFIX}
+run git pull --rebase
 run rake windows:build
 run rake cross compile native gem
 
