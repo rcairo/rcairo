85bb3401ee7f8d979db51107cae529185c07608d
Sutou Kouhei <kou@clear-code.com>
2023-09-08 12:33:58 +0900

    test: ensure closing surfaces
    GitHub: fix GH-82
    
    It's for avoiding StringIO#write calls on Ruby's GC. If surfaces
    aren't finished explicitly, they may write something when they are
    destroyed. Ruby's GC may destroy them implicitly and it may cause
    StringIO#write while GC. It's not acceptable by Ruby.
    
    Reported by Jeremy Bicha. Thanks!!!

--- test/test_tee_surface.rb
+++ test/test_tee_surface.rb
@@ -9,24 +9,26 @@ class TeeSurfaceTest < Test::Unit::TestCase
   def test_new
     output1 = StringIO.new
     device1 = Cairo::ScriptDevice.new(output1)
-    surface1 = Cairo::ScriptSurface.new(device1, 100, 200)
-    output2 = StringIO.new
-    device2 = Cairo::ScriptDevice.new(output2)
-    surface2 = Cairo::ScriptSurface.new(device2, 100, 200)
-
-    surface = Cairo::TeeSurface.new(surface1)
-    surface << surface2
-    Cairo::Context.create(surface) do |context|
-      context.move_to(15, 30)
-      context.line_to(80, 100)
-      context.stroke
-    end
-    assert_equal(<<-EOS, output1.string)
+    Cairo::ScriptSurface.create(device1, 100, 200) do |surface1|
+      output2 = StringIO.new
+      device2 = Cairo::ScriptDevice.new(output2)
+      Cairo::ScriptSurface.create(device2, 100, 200) do |surface2|
+        Cairo::TeeSurface.create(surface1) do |surface|
+          surface << surface2
+          Cairo::Context.create(surface) do |context|
+            context.move_to(15, 30)
+            context.line_to(80, 100)
+            context.stroke
+          end
+          assert_equal(<<-SCRIPT, output1.string)
 %!CairoScript
 << /content //COLOR_ALPHA /width 100 /height 200 >> surface context
 n 15 30 m 80 100 l
 stroke+
-EOS
-    assert_equal(output1.string, output2.string)
+          SCRIPT
+          assert_equal(output1.string, output2.string)
+        end
+      end
+    end
   end
 end
